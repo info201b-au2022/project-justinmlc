279de17aa048f4429f6fc0e3b1e62b6f67bf49ed
Samuel Shumye <samuelshumye@Samuels-MacBook-Air-2.local>
2022-11-14 20:33:14 -0800

    update

--- source/ChartForQuestion1.R
+++ source/ChartForQuestion1.R
@@ -1,31 +1,32 @@
-# Chart / Data Wrangling for Research Question 1: 
+# Chart / Data Wrangling for Research Question 3: 
 # How does sleep duration and quality impact university students’ academic performance?
 
+# Load in necessary packages
 library(tidyverse)
 library(ggplot2)
 library(dplyr)
+
 # Read the csv file/files
 df <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-justinmlc/main/data/potential1.csv")
-
-chart_1 <- df%>%
-  select(study.hour, what.is.your.cgpa)%>%
-  group_by(what.is.your.cgpa) %>%
+dataset_1 <- df %>% 
+  select(study.hour, what.is.your.cgpa) %>% 
+  group_by(what.is.your.cgpa) %>% 
   summarize(avgstudyhr = mean(study.hour))
 
-chart1 <- ggplot(chart_1) +
-  geom_col(
+chart1 <- ggplot(dataset_1) + 
+  geom_histogram(
     mapping = aes(
-          x = what.is.your.cgpa,
-          y = avgstudyhr, fill = what.is.your.gpa) 
-      
-    ) +
+      x = what.is.your.cgpa, 
+      y = avgsleep, fill=what.is.your.cgpa)  
+  ) + 
+  
   labs(
     x = "Cumulative GPA",
-    Y = "Average study time",
-    Title = "Students' GPA and time they spent studying",
-  )
-  
-chart1
+    y = "Average study hour", 
+    title = "Students' GPA and time they spent studying",
+  ) + 
+  theme(legend.position = "none")
+plot(chart1)
 
 find_max_avg_studyhr <- function() {
   max_avg_studyhr <- df %>% 
@@ -33,12 +34,6 @@ find_max_avg_studyhr <- function() {
     drop_na() %>% 
     group_by(what.is.your.cgpa) %>% 
     summarize(avg_studyhr = mean(study.hour, na.rm = TRUE)) %>% 
-    pull(avg_studyhr)%>% 
-    return(round(max_avg_studyhr, 1))
-  
-  
-  )+
-  theme(legend.position = "none")
-
-
-
+    pull(avg_studyhr)
+  return(round(max_avg_studyhr, 1))
+}
