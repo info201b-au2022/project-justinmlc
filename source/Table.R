22c76dbbd62239582b76e77c9e40340a80959254
Gnorezgnaw <wch19700@gmail.com>
2022-11-16 21:11:11 -0800

    table

---
+++ source/Table.R
@@ -0,0 +1,17 @@
+# Table
+library(tidyverse)
+
+df1 <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-justinmlc/main/data/potential1.csv")
+
+
+colnames(df1)[7] <- 'sleep_hour'
+colnames(df1)[9] <- 'age'
+colnames(df1)[10] <- 'cgpa'
+
+table <- df1 %>% 
+    drop_na() %>% 
+    group_by(cgpa, study.hour) %>% 
+    summarize(avg_sleep_hour = mean(sleep_hour), .groups = 'drop')
+table$avg_sleep_hour <- round(table$avg_sleep_hour, 1)
+
+
