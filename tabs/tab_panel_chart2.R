47d693724d62872601cbba16df3dc0d571c9a308
lewing19 <lewing19@uw.edu>
2022-12-07 23:11:17 -0800

    Inputted discussions, added chart 2 to server

--- tabs/tab_panel_chart2.R
+++ tabs/tab_panel_chart2.R
@@ -1,1 +1,20 @@
-# Panel for Chat 2
+# Panel for Chart 2
+
+library(shiny)
+source("source/ChartForQuestion2.R")
+
+tab_panel_chart2 <- tabPanel(
+  "Chart 2",
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("division",
+                  "Precision",
+                  min = 1/3,
+                  max = 1,
+                  value = 1/2)
+    ),
+    mainPanel(
+      plotlyOutput("chart2")
+    )
+  )
+)
